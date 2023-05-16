38a96da623e818449dbb5a2375650f392e5bcb80
Zi2le <onaopemipoojedokun@gmail.com>
2023-05-16 23:48:41 +0100

    first cmmit

---
+++ week3_ass/stress1.py
@@ -0,0 +1,23 @@
+#list and explain the differences between a list and a tuple 
+
+
+ #LIST
+# Modification
+# Format
+# Praxis
+# Mode of exploit
+
+
+# Modification:
+#                 List:
+#                        Lists are volatile, they can be changed(modified),which meanings after they are created,
+# elements can be added or removed.Values of particular elements can also be redefined.
+
+
+
+
+
+
+
+
+
